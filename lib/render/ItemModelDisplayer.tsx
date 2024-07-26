25c38b8b68b26e88584e2907051e602942d800b6
Taha Anılcan Metinyurt <igoodie@programmer.net>
2024-07-26 04:01:16 +0300

    chore: bump to v1.1.1

--- lib/render/ItemModelDisplayer.tsx
+++ lib/render/ItemModelDisplayer.tsx
@@ -33,6 +33,7 @@ export function ItemModelDisplayer(props: Props) {
     <img
       ref={ref}
       src={cachedImg}
+      hidden={!cachedImg}
       className={props.className}
       style={{ width: "100%", height: "100%" }}
     />
