b2c62622e0363db42b19acd635aad10ff02f43da
Joshua Epstein <josh@joshepstein.co.uk>
2025-07-05 16:27:00 +0100

    chore: properly update versions (hopefully)

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,36 @@
-import { resolve } from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsConfigPaths from "vite-tsconfig-paths";
-import dts from "vite-plugin-dts";
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
 // import { EsLinter, linterPlugin } from 'vite-plugin-linter'
 
 // https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
-  plugins: [
-    react(),
-    tsConfigPaths(),
-    // linterPlugin({
-    //   include: ['./src}/**/*.{ts,tsx}'],
-    //   linters: [new EsLinter({ configEnv })],
-    // }),
-    dts({
-      include: ["lib/**/*"],
-      beforeWriteFile: (filePath, content) => ({
-        filePath: filePath.replace("/lib", ""),
-        content,
-      }),
-    }),
-  ],
-  build: {
-    lib: {
-      entry: resolve("lib", "main.tsx"),
-      name: "ReactFeatureFlag",
-      formats: ["cjs", "es", "umd", "iife"],
-      fileName: (format) => `@iskallia-dev/item-model-renderer.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react"],
-    },
-  },
-}));
+export default defineConfig({
+	plugins: [
+		react(),
+		tsConfigPaths(),
+		// linterPlugin({
+		//   include: ['./src}/**/*.{ts,tsx}'],
+		//   linters: [new EsLinter({ configEnv })],
+		// }),
+		dts({
+			include: ['lib/**/*'],
+			beforeWriteFile: (filePath, content) => ({
+				filePath: filePath.replace('/lib', ''),
+				content,
+			}),
+		}),
+	],
+	build: {
+		lib: {
+			entry: resolve('lib', 'main.tsx'),
+			name: 'ReactFeatureFlag',
+			formats: ['cjs', 'es', 'umd', 'iife'],
+			fileName: (format) => `@iskallia-dev/item-model-renderer.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react'],
+		},
+	},
+});
